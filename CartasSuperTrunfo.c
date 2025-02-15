d1480db1cec484d35c1a1b64f91ca16043e1cd3d
santtanadev <68670245+shitterzin@users.noreply.github.com>
2025-02-15 00:48:29 +0000

    densidade e pibpercapita adcionados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,29 +2,38 @@
 
 int main(){
 
+    float densidade;
+    float pibper;
     double populacao;
     double area;
     double pib;
     int pontos_turisticos;
 
     printf("Qual a população da cidade?: \n");
-    scanf("%lf", &populacao);
+    scanf("%.2lf", &populacao);
 
     printf("Área (km²): ");
-    scanf("%lf", &area);
+    scanf("%.2lf", &area);
 
     printf("PIB (em bilhões): ");
-    scanf("%lf", &pib);
+    scanf("%.2lf", &pib);
 
     printf("Número de pontos turísticos: ");
     scanf("%d", &pontos_turisticos);
+    getchar();
 
 
-    printf("\nCARTA DA CIDADE\n");
+    printf(" CARTAS DE CIDADES\n");
         printf("População: %f habitantes\n", populacao);
         printf("Área: %f km²\n", area);
         printf("PIB: %f bilhões\n", pib);
         printf("Pontos turísticos: %d\n", pontos_turisticos);
+
+        densidade = populacao / area;
+        printf("Densidade Populacional: %.2f, habitantes por km²\n", densidade);
+
+        pibper = pib / populacao;
+        printf("PIB per capita: %.2f, bilhões por habitante\n", pibper);
     
 
     return 0;
